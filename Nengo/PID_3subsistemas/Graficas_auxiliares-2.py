a45392484a764fda750deef41bee970160fa87fe
A_olivares <157545091+Aolivares24@users.noreply.github.com>
2026-07-10 18:23:36 -0600

    Add files via upload
    Hi, this is my first attempt at having a debugged code.

--- Nengo/PID_3subsistemas/Graficas_auxiliares-2.py
+++ Nengo/PID_3subsistemas/Graficas_auxiliares-2.py
@@ -1,14 +1,19 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-from sklearn.preprocessing import MinMaxScaler
-from mpl_toolkits.mplot3d import Axes3D
+#from sklearn.preprocessing import MinMaxScaler
+from mpl_toolkits.mplot3d import Axes3D #axes3d
 import math
 import matplotlib.ticker as ticker
 from matplotlib.ticker import ScalarFormatter, FormatStrFormatter
+from matplotlib import rc
 
-# Configuración de fuentes
-plt.rcParams['font.family'] = 'serif'
+rc('text', usetex=True)
+rc('font', family='serif')
+
+import matplotlib.ticker as ticker
+from matplotlib.ticker import ScalarFormatter, FormatStrFormatter
+plt.rcParams['font.family'] = 'serif'  # Use a generic serif font
 plt.rcParams['font.serif'] = 'DejaVu Serif'
 
 # Leer los archivos CSV correctamente
@@ -21,6 +26,18 @@ datosZ = pd.read_csv('simulink_data_z.csv', skiprows=1, header=None)
 
 # Extraer los datos directamente (sin necesidad de str_to_float)
 # Las columnas son: [0] = tiempo, [1] = posición, [2] = velocidad
+
+# Tus archivos tienen header con 'Time,Position' y 3 columnas: tiempo, posición, velocidad
+datosInputZ = pd.read_csv('simulink_data_Uz.csv', skiprows=1, header=None)  # Saltar el header
+datosInputTheta = pd.read_csv('simulink_data_Tautheta.csv', skiprows=1, header=None)
+datosInputPhi = pd.read_csv('simulink_data_TauPhi.csv', skiprows=1, header=None)
+
+# Leer los archivos CSV correctamente
+datosInputX = pd.read_csv('simulink_data_Ux.csv', skiprows=1, header=None)  # Saltar el header
+datosInputY = pd.read_csv('simulink_data_Uy.csv', skiprows=1, header=None)
+
+
+
 
 # Datos X
 tiempo_X = datosX[0].values
@@ -49,11 +66,35 @@ PhiDotValores = datosPhi[2].values
 
 # Verificar que todos los tiempos son iguales (opcional)
 # print("Tiempos iguales:", np.allclose(tiempo_X, tiempo_Y))
+#####################################################################################################3
+# Datos In_Z
+tiempo_InputZ = datosInputZ[0].values
+InputValoresZ = datosInputZ[1].values
+
+# Datos In_Theta
+tiempo_InputTheta = datosInputTheta[0].values
+InputValoresTheta = datosInputTheta[1].values
+
+# Datos In_Phi
+tiempo_InputPhi = datosInputPhi[0].values
+InputValoresPhi = datosInputPhi[1].values
+
+#####################################################################################################3
+# Datos In_X
+tiempo_InputX = datosInputX[0].values
+InputValoresX = datosInputX[1].values
+
+# Datos In_y
+tiempo_InputY = datosInputY[0].values
+InputValoresY = datosInputY[1].values
+
+
+
 
 # Configuración de gráficos
 num_xticks = 5
 num_yticks = 5
-Grosor_linea = 3
+Grosor_linea = 2
 
 # ============================================
 # 1. GRÁFICAS DE POSICIONES TRASLACIONALES (X, Y, Z)
@@ -110,57 +151,61 @@ plt.tight_layout()
 plt.savefig("Angulos.pdf", bbox_inches='tight')
 plt.close()
 
-# # Ejemplo de cómo graficar los datos (opcional)
-# plt.figure(figsize=(12, 8))
 
-# # Graficar posiciones
-# plt.subplot(2, 2, 1)
-# plt.plot(tiempo_X, XValores, linewidth=Grosor_linea, label='X')
-# plt.plot(tiempo_Y, YValores, linewidth=Grosor_linea, label='Y')
-# plt.plot(tiempo_Z, ZValores, linewidth=Grosor_linea, label='Z')
-# plt.title('Posiciones')
-# plt.xlabel('Tiempo (s)')
-# plt.ylabel('Posición')
-# plt.legend()
-# plt.grid(True)
+# ============================================
+# 3. GRÁFICAS DE INPUTS (Uz, Tau_Theta y Tau_Phi)
+# ============================================
+fig3, (ax6, ax7, ax8) = plt.subplots(3, 1, figsize=(12, 8))
 
-# # Graficar ángulos
-# plt.subplot(2, 2, 2)
-# plt.plot(tiempo_Theta, ThetaValores, linewidth=Grosor_linea, label='Theta')
-# plt.plot(tiempo_Phi, PhiValores, linewidth=Grosor_linea, label='Phi')
-# plt.title('Ángulos')
-# plt.xlabel('Tiempo (s)')
-# plt.ylabel('Ángulo (rad)')
-# plt.legend()
-# plt.grid(True)
+# Gráfica Theta
+ax6.plot(tiempo_InputZ, InputValoresZ, linewidth=Grosor_linea, label=r'$U_z$')
+ax6.set_ylabel('Trust[N]', fontsize=28)
+ax6.grid(True, linestyle='-.')
+ax6.legend(fontsize='28', loc='upper right')
+ax6.tick_params(axis='both', labelsize=28)
 
-# # Graficar velocidades lineales
-# plt.subplot(2, 2, 3)
-# plt.plot(tiempo_X, XDotValores, linewidth=Grosor_linea, label='X_dot')
-# plt.plot(tiempo_Y, YDotValores, linewidth=Grosor_linea, label='Y_dot')
-# plt.plot(tiempo_Z, ZDotValores, linewidth=Grosor_linea, label='Z_dot')
-# plt.title('Velocidades Lineales')
-# plt.xlabel('Tiempo (s)')
-# plt.ylabel('Velocidad')
-# plt.legend()
-# plt.grid(True)
+# Gráfica Phi
+ax7.plot(tiempo_InputTheta, InputValoresTheta, linewidth=Grosor_linea, label=r'$\tau_\theta$')
+ax7.set_xlabel('Time [s]', fontsize=28)
+ax7.set_ylabel('M [Nm]', fontsize=28)
+ax7.grid(True, linestyle='-.')
+ax7.legend(fontsize='28', loc='upper right')
+ax7.tick_params(axis='both', labelsize=28)
 
-# # Graficar velocidades angulares
-# plt.subplot(2, 2, 4)
-# plt.plot(tiempo_Theta, ThetaDotValores, linewidth=Grosor_linea, label='Theta_dot')
-# plt.plot(tiempo_Phi, PhiDotValores, linewidth=Grosor_linea, label='Phi_dot')
-# plt.title('Velocidades Angulares')
-# plt.xlabel('Tiempo (s)')
-# plt.ylabel('Velocidad Angular')
-# plt.legend()
-# plt.grid(True)
+# Gráfica Y
+ax8.plot(tiempo_InputPhi, InputValoresPhi, linewidth=Grosor_linea, label=r'$\tau_\phi$')
+ax8.set_xlabel('Time [s]', fontsize=28)
+ax3.set_ylabel('M [Nm]', fontsize=28)
+ax8.grid(True, linestyle='-.')
+ax8.legend(fontsize='28', loc='upper right')
+ax8.tick_params(axis='both', labelsize=28)
 
-# plt.tight_layout()
-# plt.show()
+plt.tight_layout()
+plt.savefig("Inputs.pdf", bbox_inches='tight')
+plt.close()
 
-# # También puedes acceder a los datos como arrays de numpy
-# print(f"Datos X: {len(XValores)} puntos")
-# print(f"Datos Y: {len(YValores)} puntos")
-# print(f"Datos Z: {len(ZValores)} puntos")
-# print(f"Datos Theta: {len(ThetaValores)} puntos")
-# print(f"Datos Phi: {len(PhiValores)} puntos")
+
+# ============================================
+# 4. GRÁFICAS DE INPUTS (Ux, Uy)
+# ============================================
+fig4, (ax4, ax5) = plt.subplots(2, 1, figsize=(12, 8))
+
+# Gráfica Theta
+ax4.plot(tiempo_InputX, InputValoresX, linewidth=Grosor_linea, label=r'$U_x$')
+ax4.set_ylabel('Angle [rad]', fontsize=28)
+ax4.grid(True, linestyle='-.')
+ax4.legend(fontsize='28', loc='upper right')
+ax4.tick_params(axis='both', labelsize=28)
+
+# Gráfica Phi
+ax5.plot(tiempo_InputY, InputValoresY, linewidth=Grosor_linea, label=r'$U_y$')
+ax5.set_xlabel('Time [s]', fontsize=28)
+ax5.set_ylabel('Angle [rad]', fontsize=28)
+ax5.grid(True, linestyle='-.')
+ax5.legend(fontsize='28', loc='upper right')
+ax5.tick_params(axis='both', labelsize=28)
+
+plt.tight_layout()
+plt.savefig("InputsAuxiliar.pdf", bbox_inches='tight')
+plt.close()
+
